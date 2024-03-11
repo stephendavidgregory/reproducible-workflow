a42f8d1bc0ed8bc3e84450249491d25face3e9e8
Stephen David Gregory <stephendavidgregory@gmail.com>
2024-03-11 14:38:59 +0000

    added colour blind palette to functions

--- template-manuscript-git-v1-libsfuns.R
+++ template-manuscript-git-v1-libsfuns.R
@@ -31,3 +31,6 @@ ggplotColours <- function(n = 6, h = c(0, 360) + 15){
   if ((diff(h) %% 360) < 1) h[2] <- h[2] - 360/n
   hcl(h = (seq(h[1], h[2], length = n)), c = 100, l = 65)
 }
+
+# an alternative colour blind palette - use: scale_fill_manual(values=cbPalette), scale_colour_manual(values=cbPalette), etc.
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
